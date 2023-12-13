aacf370e36d9f5820ad88acbe2e1630d0a5f554d
AngelArielP <angel.plaza@joonx.org>
2023-12-12 18:45:43 -0600

    add boostrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import './assets/main.css'
-
+import './assets/main.css' // Importar los estilos CSS de externos
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importar los estilos CSS de Bootstrap
 
 createApp(App).mount('#app')
