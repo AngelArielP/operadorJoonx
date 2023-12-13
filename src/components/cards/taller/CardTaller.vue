006099e9ac442b1511c6c35c3682c075a00248a4
AngelArielP <angel.plaza@joonx.org>
2023-12-13 01:01:30 -0600

    add changes view taller

---
+++ src/components/cards/taller/CardTaller.vue
@@ -0,0 +1,147 @@
+<template>
+    <div class="card">
+        <div class="card-body">
+            <!-- Estructura de dos columnas -->
+            <div class="row">
+                <!-- Primera columna -->
+                <div class="col-md-6">
+                    <!-- Contenido de la primera columna -->
+                    <h6>{{ machineName }} <i class="fa-regular fa-window-restore"></i></h6>
+                    <img src="https://i.imgur.com/lgkzDTv.png" alt="maquina" width="250px" height="180">
+                </div>
+                <!-- Segunda columna -->
+                <div class="col-md-6">
+                    <!-- Contenido de la segunda columna -->
+                    <div class="container">
+                        <div class="row">
+                            <div class="col">
+                                <button class="btn btn-primary " id="labelStatus">
+                                    <p>{{ button1Text }}</p>
+                                </button>
+                            </div>
+                            <div class="col">
+                                <button class="btn btn-secondary"><i class="fa-brands fa-whmcs fa-spin"></i></button>
+                            </div>
+                            <div class="col">
+                                <button class="btn btn-success"><i class="fa-regular fa-bookmark"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Nueva fila dentro de la segunda columna -->
+                    <div class="row mt-3">
+                        <!-- Primera columna dentro de la segunda columna -->
+                        <div class="col-md-6">
+                            <!-- Contenido de la primera columna dentro de la segunda columna -->
+                            <div class="row ">
+                                <div class="col-md-12" id="txt">
+                                    <!-- Contenido de la primera fila en la primera columna -->
+                                    <p>Contenido1</p>
+                                </div>
+                            </div>
+                            <div class="row ">
+                                <div class="col-md-12" id="txt">
+                                    <!-- Contenido de la segunda fila en la primera columna -->
+                                    <p>Contenido1</p>
+                                </div>
+                            </div>
+                            <div class="row ">
+                                <div class="col-md-12" id="txt">
+                                    <!-- Contenido de la tercera fila en la primera columna -->
+                                    <p>Contenido1</p>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Segunda columna dentro de la segunda columna -->
+                        <div class="col-md-6">
+                            <!-- Contenido de la segunda columna dentro de la segunda columna -->
+                            <div class="row ">
+                                <div class="col-md-12" id="txt">
+                                    <!-- Contenido de la primera fila en la segunda columna -->
+                                    <p>Contenido1</p>
+                                </div>
+                            </div>
+                            <div class="row ">
+                                <div class="col-md-12" id="txt">
+                                    <!-- Contenido de la segunda fila en la segunda columna -->
+                                    <p>Contenido1</p>
+                                </div>
+                            </div>
+                            <div class="row ">
+                                <div class="col-md-12" id="txt">
+                                    <!-- Contenido de la tercera fila en la segunda columna -->
+                                    <p>Contenido1</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- Fila debajo de las dos columnas -->
+            <div class="row">
+                <div class="col-md-12">
+                    <!-- Contenido de la fila debajo de las columnas -->
+                    <p><i class="fa-solid fa-arrows-rotate fa-spin"></i> {{ updateText }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+  
+
+<script>
+export default {
+  props: {
+    machineName: String,
+    machineImage: String,
+    machineAlt: String,
+    button1Text: String,
+    additionalContent: Array,
+    updateText: String
+  }
+};
+</script>
+  
+<style scoped>
+/* Estilos para la tarjeta */
+.card {
+    border: 1px solid #ccc;
+    border-radius: 2px;
+    margin-top: 10px;
+    width: 610px;
+    height: 251px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+#labelStatus {
+    height: 35px;
+    width: 120px;
+    font-size: 0.7rem;
+    text-align: left;
+    padding-top: 8%;
+    background-color: #E0C17D;
+    border-radius: 1%;
+    font-weight: 900;
+
+}
+
+#txt {
+    height: 35px;
+    width: 95%;
+    margin: 2%;
+    font-size: 0.7rem;
+    text-align: left;
+    padding: 10%;
+    background-color: #F2F2F2;
+    border-radius: 10%;
+    font-weight: 900;
+}
+
+button {
+    color: black;
+    background-color: #ffffff;
+    border-color: #ffffff;
+}
+</style>
+  
--- src/pages/Taller.vue
+++ src/pages/Taller.vue
@@ -1,16 +1,39 @@
 <template>
-    <main>
-      <!-- Contenido principal -->
-        <h2>Contenido Principal</h2>
-        <p>¡Aquí va el contenido principal de tu aplicación!</p>
-      
-    </main>
-  </template>
-  
-  <script>
-  export default {
-    // Lógica del componente si es necesaria
-  }
-  </script>
+  <main>
+    <!-- Contenido principal -->
+    <div class="row">
+      <div class="col-md-6">
+        <!-- Primer CardTaller (ocupará la mitad del ancho en dispositivos medianos y grandes) -->
+        <CardTaller :machineName="'Maquina 001'" :machineImage="'https://i.imgur.com/lgkzDTv.png'"
+          :machineAlt="'Imagen de la máquina 001'" :button1Text="'Produccion lenta'"
+          :additionalContent="['Contenido1', 'Contenido2', 'Contenido3']" :updateText="'última actualizado - 2min'" />
+      </div>
+      <div class="col-md-6">
+        <!-- Segundo CardTaller (ocupará la otra mitad del ancho en dispositivos medianos y grandes) -->
+        <CardTaller :machineName="'Maquina 002'" :machineImage="'https://example.com/image.jpg'"
+          :machineAlt="'Imagen de la máquina 002'" :button1Text="'En operacion'"
+          :additionalContent="['Contenido A', 'Contenido B', 'Contenido C']" :updateText="'última actualización - 1h'" />
+      </div>
+      <div class="col-md-6">
+        <!-- Primer CardTaller (ocupará la mitad del ancho en dispositivos medianos y grandes) -->
+        <CardTaller :machineName="'Maquina 003'" :machineImage="'https://i.imgur.com/lgkzDTv.png'"
+          :machineAlt="'Imagen de la máquina 001'" :button1Text="'Produccion lenta'"
+          :additionalContent="['Contenido1', 'Contenido2', 'Contenido3']" :updateText="'última actualizado - 2min'" />
+      </div>
+      <div class="col-md-6">
+        <!-- Primer CardTaller (ocupará la mitad del ancho en dispositivos medianos y grandes) -->
+        <CardTaller :machineName="'Maquina 004'" :machineImage="'https://i.imgur.com/lgkzDTv.png'"
+          :machineAlt="'Imagen de la máquina 001'" :button1Text="'Produccion lenta'"
+          :additionalContent="['Contenido1', 'Contenido2', 'Contenido3']" :updateText="'última actualizado - 2min'" />
+      </div>
+    </div>
+  </main>
+</template>
 
-  
+<script>
+import CardTaller from '@/components/cards/taller/CardTaller.vue';
+
+export default {
+  components: { CardTaller }
+}
+</script>
